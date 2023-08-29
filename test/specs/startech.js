fe77aca69a2a2dedf7082fa3570a00b810f92a93
mahmudul006 <mahmudul.hasan@selise.ch>
2023-08-29 11:55:14 +0600

    updated

---
+++ test/specs/startech.js
@@ -0,0 +1,30 @@
+describe('Login Functionality', () => {
+  it('startech', async () => {
+    browser.url('https://www.startech.com.bd/');
+    browser.maximizeWindow();
+    const laptopLink = await $('=Laptop');
+    await laptopLink.moveTo();
+    const gamingLaptopLink = await $('=Gaming Laptop');
+    await gamingLaptopLink.moveTo();
+    const asusLaptop = await $('=Asus');
+    await asusLaptop.click();
+    const cards = await $$('.main-content .p-item');
+    await cards[1].click();
+    await browser.pause(5000);
+    // const usernameField = $('#username');
+    // const passwordField = $('#password');
+    // const loginButton = $('#login-button');
+
+    // usernameField.setValue('validUsername');
+    // passwordField.setValue('validPassword');
+    // loginButton.click();
+
+    // browser.waitUntil(() => $('#dashboard').isDisplayed(), {
+    //   timeout: 5000,
+    //   timeoutMsg: 'Dashboard did not appear after login',
+    // });
+
+    // const welcomeMessage = $('.welcome-message');
+    // expect(welcomeMessage.getText()).toEqual('Welcome, User!');
+  });
+});
